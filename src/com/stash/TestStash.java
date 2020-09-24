79b394ef105a4137e925c5b9d6b25a4a1d10907f
syntaxStudent <kobegenova1003@gmail.comwd>
2020-09-24 18:57:43 -0500

    TestStash file modified

--- src/com/stash/TestStash.java
+++ src/com/stash/TestStash.java
@@ -1,5 +1,8 @@
 package com.stash;
 
 public class TestStash {
-
+ public static void main(String [] args) {
+	 System.out.println("New modification inside our code");
+	 System.out.println("Hello world!");
+ }
 }
