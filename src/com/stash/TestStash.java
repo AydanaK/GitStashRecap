20c8791b266c7670f55976c1ffb2f4b79b2b2033
syntaxStudent <kobegenova1003@gmail.comwd>
2020-09-24 19:05:07 -0500

    jdcndns

--- src/com/stash/TestStash.java
+++ src/com/stash/TestStash.java
@@ -4,5 +4,6 @@ public class TestStash {
  public static void main(String [] args) {
 	 System.out.println("New modification inside our code");
 	 System.out.println("Hello world!");
+	 System.out.println("jfkjf");
  }
 }
