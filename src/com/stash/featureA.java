c72a146ba7d8a85fc3dbf7a37b9f3b41fd99cca0
syntaxStudent <kobegenova1003@gmail.comwd>
2020-09-24 20:29:05 -0500

    FeatureA completed

--- src/com/stash/featureA.java
+++ src/com/stash/featureA.java
@@ -5,6 +5,9 @@ public class featureA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sjcns");
+		
+		System.out.println("featureA added");
+		System.out.println("FeatureA completed");
 	}
 
 }
